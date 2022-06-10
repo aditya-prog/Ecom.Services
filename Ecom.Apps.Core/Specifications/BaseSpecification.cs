5447187964d69ec3d7ac57767b8e11fd1c4ff7f3
Aditya <ajaiswal@itron.com>
2022-06-10 12:22:15 +0530

    Creating a specification class

---
+++ Ecom.Apps.Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Apps.Core.Specifications
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+        public BaseSpecification(Expression<Func<T, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+
+        // For where condition
+        public Expression<Func<T, bool>> Criteria { get; }
+
+        // For Includes method of navigation prop
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+
+        // to add includes for diff nav property in the Includes List
+        protected void AddIncludes(Expression<Func<T, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
+    }
+}
