3562197f557cc7b3406b8e81f3cefa224e431c03
Aditya <ajaiswal@itron.com>
2022-06-11 17:53:36 +0530

    Creating Buggy test controller for errors

---
+++ Ecom.API.Rest/Controllers/BuggyController.cs
@@ -0,0 +1,61 @@
+﻿using Ecom.Apps.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ecom.API.Rest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuggyController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+
+        public BuggyController(StoreContext storeContext)
+        {
+           _storeContext = storeContext;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var thing = _storeContext.Products.Find(42);
+
+            if(thing == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            var thing = _storeContext.Products.Find(42);
+
+            var thingToReturn = thing.ToString(); // since thing will be null, so accessing ToString method
+                                                  // gives null ref error, and hence a server error
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetBadRequestForValidationError(int id)
+        {
+            // Trying to generate validation type error by sending string instead of int
+            // and that is a bad request
+            return Ok();
+        }
+
+
+    }
+}
