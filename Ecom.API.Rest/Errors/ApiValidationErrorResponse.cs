43332632249310445a4ec1c820d9c7eaa9a0398b
Aditya <ajaiswal@itron.com>
2022-06-12 19:16:57 +0530

    Improving the Validation error response

---
+++ Ecom.API.Rest/Errors/ApiValidationErrorResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Ecom.API.Rest.Errors
+{
+
+    // This validation error is checked by [ApiController] attribute ,
+    // if we  remove it validation error(bad reqest) will not come
+    public class ApiValidationErrorResponse : ApiResponse
+    {
+        public ApiValidationErrorResponse(string[] validationErrors) : base(400, "Bad request!! Parameter validation errors")
+        {
+            ValidationErrors = validationErrors;
+        }
+        // List for all the invalid parameters
+        // In IEnumerable properties, we can assign an array . Eg: ValidationErrors = string[]{'x', 'y'};
+        public IEnumerable<string> ValidationErrors { get; set; }
+    }
+}
--- Ecom.API.Rest/Startup.cs
+++ Ecom.API.Rest/Startup.cs
@@ -1,15 +1,18 @@
 using AutoMapper;
+using Ecom.API.Rest.Errors;
 using Ecom.API.Rest.Helpers;
 using Ecom.API.Rest.Middleware;
 using Ecom.Apps.Core.Interfaces;
 using Ecom.Apps.Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System.Linq;
 
 namespace Ecom.API.Rest
 {
@@ -41,6 +44,36 @@ namespace Ecom.API.Rest
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ecom.API.Rest", Version = "v1" });
+            });
+
+
+            // Below configuration is just for improving/customizing validation error response
+            // Improving means instead of returning dictionary of Validationerrors, we will flatten it out
+            // and return list of Validation errors
+
+            // Needs to configure below service for overriding [ApiController] attribute of controllers
+            // for improving validation error response
+
+            // This should be below "services.AddControllers()" else we cannot override controller behaviour
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    //  "e" is a key-value pair, we first fetch the value of that key-value pair
+                    // then fetch one ICollection prop(called errors) of that "value" , and then count it
+
+                    // Below we are trying to fetch a list/ array of all validation errors
+                    // every key is having a collection of errors, and we want to flatten it out means
+                    // create a single array of all the errors of all keys, that's why we used selectmany + select
+                    var errors = actionContext.ModelState
+                                 .Where(e => e.Value.Errors.Count > 0)
+                                 .SelectMany(e => e.Value.Errors)
+                                 .Select(x => x.ErrorMessage).ToArray();
+
+                    // Add those errors in errorResponse object
+                    var errorResponse = new ApiValidationErrorResponse(errors);
+                    return new BadRequestObjectResult(errorResponse);
+                };
             });
         }
 
